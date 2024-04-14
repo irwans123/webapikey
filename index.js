90fa4ddd4c1b37308f727e4a84039815be700fa7
irwans123 <irwansyt23@gmail.com>
2024-04-14 10:33:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ __path = process.cwd();
 var express = require('express'),
     cors = require('cors'),
     secure = require('ssl-express-www');
+const PORT = process.env.PORT || 8080 || 5000 || 3000
 var { color } = require('./lib/color.js')
 
 var mainrouter = require('./routes/main'),
@@ -18,5 +19,9 @@ app.use(express.static("public"))
 
 app.use('/', mainrouter);
 app.use('/api', apirouter);
+
+app.listen(PORT, () => {
+    console.log(color("Server running on port " + PORT,'green'))
+})
 
 module.exports = app
